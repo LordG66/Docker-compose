ef804e0f9bdb8f4889ea1fad77ffee0f99d366e4
Gabi Titarenko <gabi.ti123@gmail.com>
2020-01-26 19:15:09 +0200

    App running, added proxy @ task #6

--- lavagna/entrypoint.sh
+++ lavagna/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-CMD java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
 -Ddatasource.url="${DB_URL}" \
 -Ddatasource.username="${DB_USER}" \
 -Ddatasource.password="${DB_PASS}" \
 -Dspring.profiles.active="${SPRING_PROFILE}" \
--jar ./lavagna/lavagna/lavagna-jetty-console.war --headless
-# mvn jetty:run -DstartDBManager
+-jar /lavagna/target/lavagna-jetty-console.war --headless
