dd0e13be26129d37ae15dfcddb8f688d2a5f0c29
Gabi Titarenko <gabi.ti123@gmail.com>
2020-01-28 09:27:45 +0200

    Finished Task

--- lavagna/entrypoint.sh
+++ lavagna/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+while !wget http:///mysql:3306; do
+    sleep 1
+done
 java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
 -Ddatasource.url="${DB_URL}" \
 -Ddatasource.username="${DB_USER}" \
